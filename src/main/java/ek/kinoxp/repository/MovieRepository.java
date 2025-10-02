73555a2c55ba99fec39c7e35c1a85c91a8dd0303
ayan0138 <ayah0002@stud.ek.dk>
2025-10-02 14:58:23 +0200

    added environment variables

--- src/main/java/ek/kinoxp/repository/MovieRepository.java
+++ src/main/java/ek/kinoxp/repository/MovieRepository.java
@@ -1,4 +1,7 @@
 package ek.kinoxp.repository;
 
-public interface MovieRepository {
+import ek.kinoxp.model.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
 }
--- src/main/java/ek/kinoxp/repository/ShowRepository.java
+++ src/main/java/ek/kinoxp/repository/ShowRepository.java
@@ -4,7 +4,12 @@ import ek.kinoxp.model.Show;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface ShowRepository extends JpaRepository<Show, Long> {
+    List<Show> findByStartAtAfterOrderByStartAtAsc(LocalDateTime now); //Find alle shows hvor startAt er efter en bestemt tid, og sorter stigende efter startAt
+
 }
 
